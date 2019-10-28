2ab27c70067b63e8f2395d2af7711b2baff78ff3
Rémi Jonathan Choquette <contact@remijonathan.com>
2019-10-28 15:11:43 -0400

    Change questions and answer and Increment index

--- app/src/main/java/com/remijonathan/trivia/MainActivity.java
+++ app/src/main/java/com/remijonathan/trivia/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,63 +17,104 @@ import com.remijonathan.trivia.model.Question;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
-    /*private TextView questionText;
-    private Button backButton;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView completedIndex;
+    private TextView questionText;
+    private ImageButton backButton;
     private Button falseButton;
     private Button trueButton;
-    private Button nextButton;*/
+    private ImageButton nextButton;
 
+    List<Question> questionList;
+
+    int index;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*questionText = findViewById(R.id.question_text);
+        completedIndex = findViewById(R.id.completed_index);
+        questionText = findViewById(R.id.question_text);
         backButton= findViewById(R.id.back_button);
         falseButton = findViewById(R.id.false_button);
         trueButton = findViewById(R.id.true_button);
-        nextButton = findViewById(R.id.next_button);*/
+        nextButton = findViewById(R.id.next_button);
 
-        final String[] answer = new String[1];
 
-        final List<Question> questionList = new QuestionBank().getQuestions(new AnswerListAsyncResponse() {
+
+        questionList = new QuestionBank().getQuestions(new AnswerListAsyncResponse() {
             @Override
             public void processFinished(ArrayList<Question> questionArrayList) {
-
+                questionText.setText(questionArrayList.get(index).getAnswer());
+                completedIndex.setText(String.format("%d out of %d", index+1, questionArrayList.size()));
             }
         });
 
         int size = questionList.size();
 
-        Toast.makeText(this,Integer.toString(size), Toast.LENGTH_LONG).show();
 
-        /*backButton.setOnClickListener(this);
+
+        backButton.setOnClickListener(this);
         falseButton.setOnClickListener(this);
         trueButton.setOnClickListener(this);
         nextButton.setOnClickListener(this);
-*/
+
 
 
     }
 
 
-    /*@Override
+    @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.back_button:
                 //TODO: Back button does something.
+                //Debug
+                //Toast.makeText(this,"Back", Toast.LENGTH_SHORT).show();
+                index--;
+                updateQuestion();
                 break;
+
             case R.id.false_button:
                 //TODO: False button claims that statement is false
+                //Debug
+                //Toast.makeText(this,"False", Toast.LENGTH_SHORT).show();
+                testQuestion(false);
                 break;
+
             case R.id.true_button:
                 //TODO: True button claims that statement is true
+                //Debug
+                //Toast.makeText(this,"True", Toast.LENGTH_SHORT).show();
+                testQuestion(true);
                 break;
+
             case R.id.next_button:
                 //TODO: switches to next question
+                //Debug
+                //Toast.makeText(this,"Next", Toast.LENGTH_SHORT).show();
+                index++;
+                updateQuestion();
                 break;
         }
-    }*/
+    }
+
+    void updateQuestion(){
+        if (index < 0) index = questionList.size()-1;
+        if(index > questionList.size()-1) index = 0;
+            questionText.setText(questionList.get(index % questionList.size()).getAnswer());
+
+            completedIndex.setText(String.format("%d out of %d", (index+1), questionList.size()));
+    }
+
+    void testQuestion(boolean response){
+        if (response == questionList.get(index).isAnswerTrue()){
+            //TODO: Answer correct
+            Toast.makeText(this,"Correct", Toast.LENGTH_SHORT).show();
+        }else {
+            //TODO: Answer incorrect
+            Toast.makeText(this,"Incorrect", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
